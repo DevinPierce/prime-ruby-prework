9ccfaa449783d76f768a7e6c7c933a1f41ae154b
Devin Pierce <devinmpierce@gmail.com>
2018-05-09 17:34:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
 def prime?(n)
-  n == 2 ? return true : nil
-  n < 2 ? return false : nil
+  if (n == 2)
+    return true
+  end
+  if (n < 2)
+    return false
+  end
   range = (2...n)
   for num in range do
     if (n % num == 0)
